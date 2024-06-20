5530b5d325c8cf564cb71b584e34de74bccdc534
Carl Huang <carl.huang@pstarc.com>
2024-06-20 09:36:44 +0800

    main(main): 123
    需求
    
    123
    
    調整
    
    123

--- test.sh
+++
@@ -1,9 +0,0 @@
-git remote add pstarc https://github.com/ChecheHuang/test1.git
-git pull --allow-unrelated-histories pstarc
-git checkout v2
-git checkout main
-git merge -n --allow-unrelated-histories v2
-git checkout --theirs .
-git add .
-git commit -m "test"
-git remote remove pstarc
